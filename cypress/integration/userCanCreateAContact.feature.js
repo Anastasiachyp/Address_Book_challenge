57128ace57aea3cea7b2695373553755aef2a2f4
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-15 15:17:29 +0100

    pass all tests in cypress

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -2,5 +2,11 @@ describe('user can create a contact', () => {
 	it('test', () => {
         cy.visit('http://localhost:3001')
         cy.get('#add-contact').click()
+        cy.get('#name').type('Anastasiya')
+		cy.get('#email').type('chypyhaanastasia@gmail.com')
+		cy.get('#phone').type('0700 101010')
+		cy.get('#company').type('Craft Academy')
+		cy.get('#notes').type('Full Stack developer student')
+		cy.get('#twitter').type('@anastasiachypyha')
 	})
 })
